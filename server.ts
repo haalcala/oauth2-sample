d0013226ca8ad3cbd0287c966bb261ee36fcefb3
Harold Alcala <harold.alcala@vcube.co.jp>
2019-05-16 12:52:15 +0800

    Initial import

--- server.ts
+++ server.ts
@@ -20,31 +20,31 @@ app.use(express.static(__dirname + "/public"));
 
 // Declare the redirect route
 app.get("/oauth/redirect", (req, res) => {
-	// The req.query object has the query params that
-	// were sent to this route. We want the `code` param
+  // The req.query object has the query params that
+  // were sent to this route. We want the `code` param
 
-	console.log("/oauth/redirect req", req, "res", res);
+  console.log("/oauth/redirect req", req, "res", res);
 
-	const requestToken = req.query.code;
-	axios({
-		// make a POST request
-		method: "post",
-		// to the Github authentication API, with the client ID, client secret
-		// and request token
-		url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-		// Set the content type header, so that we get the response in JSOn
-		headers: {
-			accept: "application/json"
-		}
-	}).then(response => {
-		// Once we get the response, extract the access token from
-		// the response body
-		const accessToken = response.data.access_token;
-		// redirect the user to the welcome page, along with the access token
-		res.redirect(`/welcome.html?access_token=${accessToken}`);
-	});
+  const requestToken = req.query.code;
+  axios({
+    // make a POST request
+    method: "post",
+    // to the Github authentication API, with the client ID, client secret
+    // and request token
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSOn
+    headers: {
+      accept: "application/json"
+    }
+  }).then(response => {
+    // Once we get the response, extract the access token from
+    // the response body
+    const accessToken = response.data.access_token;
+    // redirect the user to the welcome page, along with the access token
+    res.redirect(`/welcome.html?access_token=${accessToken}`);
+  });
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 
 console.log("Now listening on port 8080");
